5102aa8d9bc52e1afdcb4c5d415bff06ffd33ebf
Cláudio Silva <claudio.engdist@gmail.com>
2024-01-31 00:52:37 -0300

    presenting tokens in cards

--- app/components/tvl/Tvl.tsx
+++ app/components/tvl/Tvl.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import getTVL from './CartesiRollupsTVL.js';
+import { Box, Card, CardBody, Flex, Stat, StatHelpText, StatLabel, StatNumber, Text } from '@chakra-ui/react';
 
 interface TvlProps {
   tvl: number;
@@ -8,20 +9,29 @@ interface TvlProps {
 }
 
 const Tvl = async () => {
-  //const {tvl, erc20TokensInfo, erc20Prices} = await getTVL() as TvlProps;
-  const tvl=10;
-  const erc20TokensInfo: { [key: string]: { symbol: string } } = { "a": {"symbol":"CTSI"} };
-  const erc20Prices: { [key: string]: number } = { "a": 1 };
+  const {tvl, erc20TokensInfo, erc20Prices} = await getTVL() as TvlProps;
+  //const tvl=10;
+  //const erc20TokensInfo: { [key: string]: { symbol: string } } = { "a": {"symbol":"CTSI"}, "b":{"symbol":"weth"} };
+  //const erc20Prices: { [key: string]: number } = { "a": 1, "b":2 };
 
   return (
-    <div>
-      <p>${tvl}</p>
-      <p className="value-subtitle">* Using CoinMarketCap API to retrieve tokens price in US$</p>
-      <hr/>
-      {Object.keys(erc20TokensInfo).map((token: string) => (
-        <p className="value-subtitle" key={token}>{erc20TokensInfo[token].symbol}: ${erc20Prices[token]}</p>
-      ))}
-    </div>
+    <Box>
+      <Stat>
+        <StatLabel>Ethereum Mainnet</StatLabel>
+        <StatNumber>US${tvl}</StatNumber>
+        <StatHelpText>* Using CoinMarketCap API to retrieve tokens price</StatHelpText>
+      </Stat>
+      
+      <Flex direction="row" flexWrap="wrap">
+        {Object.keys(erc20TokensInfo).map((token: string) => (
+          <Card key={token} bg="blue.400" m="2">
+            <CardBody>
+              <Text color="white">{erc20TokensInfo[token].symbol}</Text>
+            </CardBody>
+          </Card>          
+        ))}
+      </Flex>
+    </Box>
   )
 }
 
