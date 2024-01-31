730f353dda9b8ac786332f78f7b2b709deb6c6e0
Cláudio Silva <claudio.engdist@gmail.com>
2024-01-30 23:01:51 -0300

    fix for the build

--- app/components/tvl/Tvl.tsx
+++ app/components/tvl/Tvl.tsx
@@ -3,19 +3,18 @@ import getTVL from './CartesiRollupsTVL.js';
 
 interface TvlProps {
   tvl: number;
-  erc20TokensInfo:  { [key:string] : any; };
-  erc20Prices:      { [key: string]: number; };
+  erc20TokensInfo:  { [key: string]: { symbol: string } };
+  erc20Prices:      { [key: string]: number };
 }
 
 const Tvl = async () => {
   //const {tvl, erc20TokensInfo, erc20Prices} = await getTVL() as TvlProps;
   const tvl=10;
-  const erc20TokensInfo = { "a": {"symbol":"CTSI"} };
-  const erc20Prices = { "a": 1 };
+  const erc20TokensInfo: { [key: string]: { symbol: string } } = { "a": {"symbol":"CTSI"} };
+  const erc20Prices: { [key: string]: number } = { "a": 1 };
 
   return (
     <div>
-      <h2>Sum of values locked in each Cartesi Rollups DApp on Ethereum Mainnet</h2>
       <p>${tvl}</p>
       <p className="value-subtitle">* Using CoinMarketCap API to retrieve tokens price in US$</p>
       <hr/>
