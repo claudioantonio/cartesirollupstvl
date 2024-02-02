0dcbce9ee3524902786c422b1bcce27b344f6105
Cláudio Silva <claudio.engdist@gmail.com>
2024-02-01 23:12:59 -0300

    Adding Link to github repo

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, VStack } from '@chakra-ui/react'
+import { Box, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { FaGithub } from 'react-icons/fa'
 
@@ -7,7 +7,11 @@ const Footer = () => {
     <Box as="footer">
         <VStack spacing="8">
             <Text pt="12">Made in Brazil</Text>
-            <Box pb="12"><FaGithub size="30"/></Box>
+            <Box pb="12">
+              <Link href="https://github.com/claudioantonio/cartesirollupstvl">
+                <FaGithub size="30"/>
+              </Link>
+            </Box>
         </VStack>
     </Box>
   )
